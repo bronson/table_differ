fa7155dfce11aa4226528a7358977d2ed5ee3a8b
Scott Bronson <brons_git@rinspin.com>
2014-06-22 21:50:27 -0700

    be more consistent with snapshot names

--- lib/table_differ.rb
+++ lib/table_differ.rb
@@ -10,20 +10,28 @@ module TableDiffer
 
   module ClassMethods
     # pass a date, receive a value worthy of being a table name
-    def snapshot_name date
-      date.strftime("%Y%m%d_%H%M%S")
+    def snapshot_name name
+      if name.kind_of?(Date) || name.kind_of?(Time)
+        name = name.strftime("%Y%m%d_%H%M%S")
+      end
+
+      unless name.index(table_name) == 0
+        name = "#{table_name}_#{name}"
+      end
+
+      name
     end
 
     def snapshots
       connection.tables.grep(/^#{table_name}_/).sort
     end
 
-    def create_snapshot name=snapshot_name(Time.now)
-      connection.execute("CREATE TABLE #{table_name + '_' + name} AS SELECT * FROM #{table_name}")
+    def create_snapshot name=Time.now
+      connection.execute("CREATE TABLE #{snapshot_name(name)} AS SELECT * FROM #{table_name}")
     end
 
     def delete_snapshot name
-      connection.execute("DROP TABLE #{table_name + '_' + name}")
+      connection.execute("DROP TABLE #{snapshot_name(name)}")
     end
 
     def delete_snapshots &block
